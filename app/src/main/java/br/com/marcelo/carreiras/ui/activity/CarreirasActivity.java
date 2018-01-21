6b9e344988944331cbb202fcdf1c1edf0ba15dff
esoft41 - Marcelo Banhos <marcelobanhoss@gmail.com>
2018-01-20 23:24:57 -0200

    Iniciando o layout

---
+++ app/src/main/java/br/com/marcelo/carreiras/ui/activity/CarreirasActivity.java
@@ -0,0 +1,15 @@
+package br.com.marcelo.carreiras.ui.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import br.com.marcelo.carreiras.R;
+
+public class CarreirasActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_carreiras);
+    }
+}
